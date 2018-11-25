296cbb627dfe45f4d4e66a4b81fc231a099fcfe6
Varun <varun@skcript.com>
2018-11-25 16:01:51 +0530

    New Config

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,6 +9,7 @@ CHAINCODE_VERSION="21.0"
 CHANNEL_NAME=mychannel
 ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 ORDERER_ENDPOINT=orderer.example.com:7050
+
 # Install chaincode in Org1
 docker exec cli peer chaincode install -n $CHAINCODE_NAME -p $CHAINCODE_SRC -v $CHAINCODE_VERSION
 
@@ -16,4 +17,4 @@ docker exec cli peer chaincode install -n $CHAINCODE_NAME -p $CHAINCODE_SRC -v $
 docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp" -e "CORE_PEER_ADDRESS=peer0.org2.example.com:7051" cli peer chaincode install -n $CHAINCODE_NAME -p $CHAINCODE_SRC -v $CHAINCODE_VERSION
 
 # Instantiate chaincode in Org1 Channel
-docker exec cli peer chaincode upgrade -o $ORDERER_ENDPOINT --tls --cafile $ORDERER_CA -C $CHANNEL_NAME -c '{"Args":["init"]}' -n $CHAINCODE_NAME -v $CHAINCODE_VERSION -P "OR ('Org1MSP.member', 'Org2MSP.member')"
+docker exec cli peer chaincode instantiate -o $ORDERER_ENDPOINT --tls --cafile $ORDERER_CA -C $CHANNEL_NAME -c '{"Args":["init"]}' -n $CHAINCODE_NAME -v $CHAINCODE_VERSION -P "OR ('Org1MSP.member', 'Org2MSP.member')"
