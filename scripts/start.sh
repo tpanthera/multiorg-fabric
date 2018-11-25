c9f609d3af011d3772a8540774ac5468aa2b5b56
Varun <varun@skcript.com>
2018-11-25 15:48:06 +0530

    New Config

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,7 +10,7 @@ docker-compose -f docker-compose.yml up -d
 # wait for Hyperledger Fabric to start
 
 # incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
-export FABRIC_START_TIMEOUT=30
+export FABRIC_START_TIMEOUT=15
 #echo ${FABRIC_START_TIMEOUT}
 sleep ${FABRIC_START_TIMEOUT}
 
